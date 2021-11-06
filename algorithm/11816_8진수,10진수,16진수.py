8eea2d0d4d176989bf4fd2e7e9925920688b26bf
Jaeyun <v44ads@naver.com>
2021-11-06 09:06:30 +0900

    algorithm modified

--- algorithm/11816_8진수,10진수,16진수.py
+++ algorithm/11816_8진수,10진수,16진수.py
@@ -3,11 +3,9 @@ import sys
 prob = sys.stdin.readline().strip()
 
 if prob[1] != 'x' and prob[0] == '0':
-    temp = ''.join(prob)
-    print(int(temp, base=8))
+    print(int(prob, base=8))
 elif prob[0] == '0':
-    temp = ''.join(prob)
-    print(int(temp, base=16))
+    print(int(prob, base=16))
 else:
     print(int(prob))
 
